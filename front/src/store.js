f148a41d65a5044b8aa759fc9a74bd24fd0be39c
Denis Irkhin <jscriptor5@gmail.com>
2019-03-31 22:20:00 -0400

    Update packages to actual versions. Bring thunk

--- front/src/store.js
+++ front/src/store.js
@@ -1,78 +1,20 @@
-import { createStore } from 'redux'
-// import reducer from './reducers/reducer'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 // In index.js we exported combineReducers variable, but since it was default export we can name it here as we want
 import rootReducers from './reducers/index'
 
-// const reducer = function (state, action) {
-//   console.log('dispatch triggered')
-//   switch (action.type) {
-//     case 'login':
-//       console.log('action login used')
-//       return {
-//         ...state,
-//         state: { ...state.state, loggedIn: true, user: action.payload.email, userId: action.payload.userId, avatar: action.payload.avatar }
-//       }
-
-//     case 'logout':
-//       if (document.cookie.includes('__sid__')) {
-//         document.cookie = '__sid__=""'
-//         // document.location.reload()
-//       }
-//       // document.location.reload()
-//       console.log('action logout used')
-//       return {
-//         ...state,
-//         state: { ...state.state, loggedIn: false, user: 'not logged in', userId: '', avatar: '', lists: [] }
-//       }
-
-//     case 'getLists':
-//       console.log('action getLists used')
-//       return { ...state, state: { ...state.state, lists: action.payload } }
-
-//     case 'searchList':
-//       console.log('action searchList used')
-//       return {
-//         ...state,
-//         state: { ...state.state, searchListQuery: action.payload }
-//       }
-
-//     case 'editList':
-//       console.log('action editList used')
-//       return {
-//         ...state,
-//         state: { ...state.state, editList: action.payload }
-//       }
-
-//     case 'get-user-profile':
-//       console.log('Reduce Get-User-Profile')
-//       return {
-//         ...state,
-//         state: { ...state.state, profile: action.payload, isProfileLoaded: true }
-//       }
-
-//     default:
-//       return { state }
-//   }
-// }
-
 // Root level of Store is empty. Each reducer has own initialState
 const initialState = {}
-// const initialState = {
-//   loggedIn: false,
-//   user: '',
-//   userId: '',
-//   avatar: '',
-//   profile: null,
-//   isProfileLoaded: false
 
-//   // lists: []
-// }
+const middleware = [thunk]
 
 const store = createStore(
   rootReducers, // reducer
   initialState, // InitialState
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose( // Middleware and Redux dev tools extension. Compose - https://redux.js.org/api/compose
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 export default store
