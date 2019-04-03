470947827552a1704eed068a8c9d7ab393551aad
Denis Irkhin <jscriptor5@gmail.com>
2019-04-03 17:14:34 -0400

    Set dev db to ml-test

--- back/config/param_dev.js
+++ back/config/param_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-  MONGO_DO: 'mongodb://dbUser:TsTQNyBwykyjBegXv8PaUUWtu@mng.movielists.ca:27017/ml',
+  MONGO_DO:
+    'mongodb://testUser:msPDVnaAsNmmXkXQb8@mng.movielists.ca:27017/ml-test',
   CORS: 'http://localhost:3080',
   SERVER_PORT: 5050,
   JWT_SECRET: 'HgkT8FmXGCBqznszqjsYDsrKx'
