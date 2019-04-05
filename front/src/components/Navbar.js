4ef782218b057f9d9bb604c60a3bb473d6d40ec4
Denis Irkhin <jscriptor5@gmail.com>
2019-04-05 15:33:53 -0400

    Test CD netlify again

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -308,7 +308,7 @@ class Navbar extends Component {
 
             <li className="nav-item">
               <Link to="/premium" className="nav-link">
-                PREMIUMSS
+                PREMIUM
               </Link>
             </li>
           </ul>
