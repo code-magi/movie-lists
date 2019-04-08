6d4f8df9b7cf8e3987bc102cb44be205b542eccc
Denis Irkhin <jscriptor5@gmail.com>
2019-04-08 13:06:59 -0400

    Test06 to hide dev param file

--- back/config/param_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGO_DO:
-    'mongodb://testUser:msPDVnaAsNmmXkXQb8@mng.movielists.ca:27017/ml-test',
-  CORS: 'http://localhost:3080',
-  SERVER_PORT: 5050,
-  JWT_SECRET: 'HgkT8FmXGCBqznszqjsYDsrKx'
-}
